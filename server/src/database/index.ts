ab673963f629dec5734bd9219516feb49f5c1f1a
Taofeek <ibrahimtaofeek005@gmail.com>
2024-04-24 15:48:05 +0100

    Fix: Server not working yet but built and kept

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,12 +1,12 @@
+require('dotenv').config();
 import mongoose from "mongoose";
-import {MONGO_URL} from "../configs";
 
 export const connectDatabase = async () => {
-    const connect = await mongoose.connect(`${MONGO_URL}`, {
+    const connect = await mongoose.connect(`${process.env.MONGO_URL}`, {
         // //useNewUrlParser: true,
         // useCreateIndex: true,
         // useUnifiedTopology: true
     });
 
-    console.log(`MongoDB connected: ${MONGO_URL}`)
+    console.log(`MongoDB connected: ${process.env.MONGO_URL}`)
 }
